3a8cd18ee1e77612fb3fc2c91350f62614b1b99b
Gaurav Saraswat <Gaurav Saraswat>
2015-11-24 23:34:42 +0530

    Myserver.js

--- Myserver.js
+++ Myserver.js
@@ -8,12 +8,12 @@ Io.on('connection',function(socket){
 	console.log('A new user has got connected!!!');     //On new user connected display on console that new user has got connected
 	socket.on('chat message',function(msg){
 		//console.log('message:'+msg);
-		Io.emit('chat message', msg);
+		Io.emit('chat message', msg);                   //On receiving an object(it contains sender's name and his message),broadcast it to all connected clients
 	});
-	socket.on('disconnect', function(){
+	socket.on('disconnect', function(){                 // whenever a client/user disconnects(refresh counts for disconnect followed by  connect again)
 		console.log('Some user has disconnected');
 	});
 })
-Http.listen(3000,function(){
+Http.listen(3000,function(){                            //Using port  3000 for running server
 	console.log('I am listening on*:3000');
 });
